b6870e7effcb591a2cc310022634a200a3cf5ba6
foxi.chen <foxi.chen@mljr.com>
2020-07-22 22:22:01 +1200

    new project

---
+++ back-service/src/main/java/org/wucc/backservice/BackServiceApplication.java
@@ -0,0 +1,13 @@
+package org.wucc.backservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackServiceApplication.class, args);
+    }
+
+}
